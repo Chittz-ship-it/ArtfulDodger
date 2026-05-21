a446be258186ecaebf396dbe15ca0c73858c17bf
Chittz-ship-it <charliechitty@hotmail.co.uk>
2026-05-21 23:14:03 +0100

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PennyPickerDashboard from './PennyPickerDashboard.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PennyPickerDashboard />
+  </React.StrictMode>
+)
